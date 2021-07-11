c3a2ed3a6894e2aed00896c78b79b79724795529
Douglas Voltan <voltan2015@gmail.com>
2021-07-11 20:57:18 -0300

    Projeto inicial Ola Mundo

---
+++ projetogit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projetogit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo");
+        }
+    }
+}
